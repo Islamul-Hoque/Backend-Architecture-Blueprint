f011290f4be8926a6a891cc500e29bbfad9270ac
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-18 02:46:48 +0600

    fact(route.ts): implement route handler with root GET route, product controller, and 404 response

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,7 @@ import { createServer, IncomingMessage, Server } from "http";
 import { routeHandler } from "./routes/route";
 
 const server: Server = createServer((req: IncomingMessage, res)=>{
-    const url = req.url;
-        const method = req.method
-    
-        if (url == "/" && method == "GET") {
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(JSON.stringify({ message: "This is Root route" }));
-        
-        } else {
-            res.writeHead(404, { "content-type": "application/json" });
-            res.end(JSON.stringify({ message: "Route not found!" }));
-        }
+    routeHandler(req, res)
 })
 
 server.listen(5000, ()=>{
