8bdbd0952aa2c12baeec1b6303cd141042b50c0f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-18 02:45:37 +0600

    fact(server.ts): add basic http server with root GET route and 404 handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
-// import { createServer, IncomingMessage, Server } from "http";
-// import { routeHandler } from "./routes/route";
+import { createServer, IncomingMessage, Server } from "http";
+import { routeHandler } from "./routes/route";
 
-// const server: Server = createServer((req: IncomingMessage, res)=>{
-//     routeHandler(req, res)
-// })
+const server: Server = createServer((req: IncomingMessage, res)=>{
+    const url = req.url;
+        const method = req.method
+    
+        if (url == "/" && method == "GET") {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(JSON.stringify({ message: "This is Root route" }));
+        
+        } else {
+            res.writeHead(404, { "content-type": "application/json" });
+            res.end(JSON.stringify({ message: "Route not found!" }));
+        }
+})
 
-// server.listen(5000, ()=>{
-//     console.log(`Server is running on the port: 5000`);
-// })
+server.listen(5000, ()=>{
+    console.log(`Server is running on the port: 5000`);
+})
