fbb6aecc083a85076cb075a3e7601512f3d4695c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-19 02:54:28 +0600

    fact(parseBody.ts): implement parseBody utility to handle and parse incoming request body as JSON

--- src/utility/parseBody.ts
+++ src/utility/parseBody.ts
@@ -1,18 +1,18 @@
-// import type { IncomingMessage } from "http"
+import type { IncomingMessage } from "http"
 
-// export const parseBody = (req: IncomingMessage): promise<any>=>{
-//     return new Promise((resolve, reject)=>{
-//         let body = ""
-//         req.on("data", (chunk)=>{
-//             body +=chunk;
-//         })
+export const parseBody = (req: IncomingMessage): promise<any>=>{
+    return new Promise((resolve, reject)=>{
+        let body = ""
+        req.on("data", (chunk)=>{
+            body +=chunk;
+        })
 
-//         req.on("end", ()=>{
-//             try {
-//                 resolve(JSON.parse(body));
-//             } catch (error) {
-//                 reject(error);
-//             }
-//         })
-//     })
-// }
+        req.on("end", ()=>{
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                reject(error);
+            }
+        })
+    })
+}
