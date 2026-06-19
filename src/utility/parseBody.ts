43d934c972724f400cb51df3d4b24295265138dc
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-20 04:30:34 +0600

    fix(parseBody.ts): correct return type to Promise<any> for parseBody utility

--- src/utility/parseBody.ts
+++ src/utility/parseBody.ts
@@ -1,6 +1,6 @@
 import type { IncomingMessage } from "http"
 
-export const parseBody = (req: IncomingMessage): promise<any>=>{
+export const parseBody = (req: IncomingMessage): Promise<any>=>{
     return new Promise((resolve, reject)=>{
         let body = ""
         req.on("data", (chunk)=>{
